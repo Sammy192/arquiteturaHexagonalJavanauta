5b8ffef8e2836d528fa7e9129e351719b831cd2c
Samuel Rodrigues Borges <samuel.borges@e-unicred.com.br>
2025-03-21 14:53:39 -0300

    projeto criado

---
+++ src/main/java/com/javanauta/operadoracartaocredito/OperadoraCartaoCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.operadoracartaocredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperadoraCartaoCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperadoraCartaoCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/operadoracartaocredito/OperadoraCartaoCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.operadoracartaocredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperadoraCartaoCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
